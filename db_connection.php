8fc91c7fdf7e197516feed8339521935e5850daf
akmaurya31 <akmaurya31@gmail.com>
2025-04-27 14:18:49 +0530

    add

--- db_connection.php
+++ db_connection.php
@@ -18,7 +18,7 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected successfully!";
+   // echo "Connected successfully!";
 }
 
 function getNextCid($conn) {
